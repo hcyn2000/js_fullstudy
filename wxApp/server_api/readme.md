1afffe48cfb1710479895dcf37cbd7d451cef431
hcyn2000 <1520090739@qq.com>
2020-07-23 10:12:21 +0800

    提交

---
+++ wxApp/server_api/readme.md
@@ -0,0 +1,10 @@
+给小程序配上后端接口
+1. npm init -y
+    JS 一个人   GO语言
+2. node index.js     入口文件
+3. yarn facebook 包管理器
+4. nodemon 
+5. yarn global add ndemon
+    npm i -g modemon
+    npm rum dev     tarn dev
+6. 启动服务器
--- wxApp/shopping/pages/cart/cart.js
+++ wxApp/shopping/pages/cart/cart.js
@@ -5,42 +5,73 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    carts: [],
+    totalPrice: 0,
+    selectAllStatus: true
   },
+  selectList(e) {
+    const index = e.currentTarget.dataset.index;  // 获取data- 传进来的index
+    let carts = this.data.carts;                  // 获取购物车列表
+    const selected = carts[index].selected;          // 获取当前商品的选中状态
+    carts[index].selected = !selected;              // 改变状态
+    this.setData({
+      carts: carts
+    });
+  },
+  selectAll(e) {
+    let selectAllStatus = this.data.selectAllStatus;    // 是否全选状态
+    selectAllStatus = !selectAllStatus;
+    let carts = this.data.carts;
 
+    for (let i = 0; i < carts.length; i++) {
+      carts[i].selected = selectAllStatus;            // 改变所有商品状态
+    }
+    this.setData({
+      selectAllStatus: selectAllStatus,
+      carts: carts
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onLoad');
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log('onReady');
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log('onShow');
+    setTimeout(() => {
+      this.setData({
+        carts: [
+          { id: 1, title: '新鲜芹菜 半斤', image: '/image/s5.png', num: 4, price: 0.01, selected: true },
+          { id: 2, title: '素米 500g', image: '/image/s6.png', num: 1, price: 0.03, selected: true }
+        ]
+      })
+    }, 1000)
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('onHide');
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log('onUnload');
   },
 
   /**
