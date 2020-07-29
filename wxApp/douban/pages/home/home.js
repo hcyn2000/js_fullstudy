e79ba6c3d6b353d0166f6a0d6a6eac17bdfd572b
hcyn2000 <1520090739@qq.com>
2020-07-29 21:22:14 +0800

    提交

--- wxApp/douban/pages/home/home.js
+++ wxApp/douban/pages/home/home.js
@@ -5,32 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
-    inputShowed: false,
-    inputVal: ""
+
   },
 
-  onLoad() {
-    this.setData({
-      search: this.search.bind(this)
-    })
-  },
-  search: function (value) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
-      }, 200)
-    })
-  },
-  selectResult: function (e) {
-    console.log('select result', e.detail)
-  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCity((city) => {
+      this.loadData(0, { city: city, apikey: '0df993c66c0c636e29ecbb5344252a4a' })
+    })
+  },
+  loadData(idx, params) {
+    let url = wx.db.url('/v2/movie/in_theaters')
+    wx.request({
+      url: url,
+      data: params,
+      header: { 'content-type': 'json' },
+      success: (res) => {
+        console.log(res);
+      }
+    })
   },
 
+  getCity(succeed) {
+    //拿到当前所在的城市名称
+    //1.先拿到当前所在区域的经纬度（小程序自带的一个AIP）
+    wx.getLocation({
+      success: (res) => {
+        // console.log(res);
+        //将经纬度转换成具体的名称
+        wx.request({
+          url: 'https://api.map.baidu.com/reverse_geocoding/v3',
+          data: {
+            ak: 'AgstQBQMuypsnELAm0YdNXfSjdHC9Xld',
+            output: 'json',
+            coordtype: 'wgs84ll',
+            location: `${res.latitude},${res.longitude}`
+          },
+          success: (res) => {
+            // console.log(res);
+            let city = res.data.result.addressComponent.city
+            succeed && succeed(city)
+          },
+          fail: () => {
+            console.log('获取城市失败');
+          }
+        })
+      },
+      fail: () => { }
+    });
+
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
---
+++ wxApp/sanyuan_Music/readme.md
@@ -0,0 +1,13 @@
+- 数据来源
+  1. 有开源接口
+    - banner
+    - personalized
+
+  2. 没有就做假数据 mocker.js
+  data:{
+      list:[
+          {
+            key:val 
+      }
+      ]
+  }
