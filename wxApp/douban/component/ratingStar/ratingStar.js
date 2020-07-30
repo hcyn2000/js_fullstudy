3c37ba43b8685096afc17b7f97ff02ee1d8bf5cd
hcyn2000 <1520090739@qq.com>
2020-07-30 21:48:36 +0800

    提交

--- wxApp/douban/component/ratingStar/ratingStar.js
+++ wxApp/douban/component/ratingStar/ratingStar.js
@@ -1,12 +1,35 @@
 // component/ratingStar/ratingStar.js
+const STAR_ON = "/assets/imgs/rating_star_small_on.png";
+const STAR_OFF = "/assets/imgs/rating_star_small_off.png";
+const STAR_HALF = "/assets/imgs/rating_star_small_half.png";
+
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
     score: {
+      type: Number,
+      observer: function (newVal, oldVal, path) {
+        let stars = [STAR_OFF, STAR_OFF, STAR_OFF, STAR_OFF, STAR_OFF]
+        if (newVal > 0) {
+          newVal = newVal / 10
+          let floor = Math.floor(newVal)
+          if (floor != newVal) { // 说明有小数
+            stars[floor] = STAR_HALF
+          }
+          for (let i = 0; i < floor; i++) {
+            stars[i] = STAR_ON
+          }
+        }
+        this.setData({
+          stars: stars
+        })
+      }
+    },
+    iconSize: {
       type: String,
-      value: 0
+      value: '26rpx'
     }
   },
 
@@ -14,20 +37,9 @@ Component({
    * 组件的初始数据
    */
   data: {
-    grade: 0,
-    harf: 0,
-    off: 0
+    stars: [STAR_OFF, STAR_OFF, STAR_OFF, STAR_OFF, STAR_OFF]
   },
-  lifetimes: { // 组件生命周期声明对象，将组件的生命周期收归到该字段进行声明，原有声明方式仍旧有效，如同时存在两种声明方式，则lifetimes字段内声明方式优先级最高
-    attached: function () { // 在组件实例进入页面节点树时执行
-      // console.log(this.data.score)
-      this.setData({
-        grade: Math.floor(this.data.score),
-        harf: this.data.score - this.data.grade,
-        off: Math.floor(5 - this.data.score)
-      })
-    }
-  },
+
   /**
    * 组件的方法列表
    */
