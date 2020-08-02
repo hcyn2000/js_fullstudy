4897d34bd94d1c14f11a530123a49aa654c37352
hcyn2000 <1520090739@qq.com>
2020-08-02 17:18:01 +0800

    提交

--- es6/map/index.js
+++ es6/map/index.js
@@ -1,70 +1,70 @@
-// let obj = {
-//     name: '红尘',
-//     age: 18,
-//     sex: 'man'
-// }
-// obj.height = '111cm'
-// const m = new Map()
-// console.log(m);      //Map{}
-// m.set(obj, 'content')
-// console.log(m); //Map(1) { { name: '红尘', age: 18, sex: 'man' } => 'content' }
+let obj = {
+    name: '红尘',
+    age: 18,
+    sex: 'man'
+}
+obj.height = '111cm'
+const m = new Map()
+console.log(m);      //Map{}
+m.set(obj, 'content')
+console.log(m); //Map(1) { { name: '红尘', age: 18, sex: 'man' } => 'content' }
 
-// console.log(m.get(obj));    //content
+console.log(m.get(obj));    //content
 
-// console.log(m.has(obj));    //true  查看是否存在obj
+console.log(m.has(obj));    //true  查看是否存在obj
 
-// m.delete(obj)   //删除obj
-
-
-// const map = new Map([
-//     ['name', '哈哈哈'],
-//     ['age', '14']
-// ])
-// console.log(map);   //Map(2) { 'name' => '哈哈哈', 'age' => '14' }
-// console.log(map.has('name'));       //true
-// console.log(map.get('name'));       //哈哈哈
-// const items = [
-//     ['name', '哈哈哈'],
-//     ['age', '14']
-// ]
-// const map = new Map()
-// items.forEach(([key, walue]) => {
-//     map.set(key, walue)
-// })
-// console.log(map);   //Map(2) { 'name' => '哈哈哈', 'age' => '14' }
+m.delete(obj)   //删除obj
 
 
-// const set = new Set([
-//     ['foo', 1],
-//     ['bar', 2]
-// ])
-// const m1 = new Map(set)
-// // console.log(set);   //Set(2) { [ 'foo', 1 ], [ 'bar', 2 ] }
-// console.log(m1);    //Map(2) { 'foo' => 1, 'bar' => 2 }
+const map = new Map([
+    ['name', '哈哈哈'],
+    ['age', '14']
+])
+console.log(map);   //Map(2) { 'name' => '哈哈哈', 'age' => '14' }
+console.log(map.has('name'));       //true
+console.log(map.get('name'));       //哈哈哈
+const items = [
+    ['name', '哈哈哈'],
+    ['age', '14']
+]
+const map = new Map()
+items.forEach(([key, walue]) => {
+    map.set(key, walue)
+})
+console.log(map);   //Map(2) { 'name' => '哈哈哈', 'age' => '14' }
 
 
-// const m = new Map()
-// const k1 = ['a']
-// const k2 = ['a']
-// m.set(k1, 111).set(k2, 222)   //k1和k2值相同，在Map中也被认为是两个键
-// console.log(m);     //Map(2) { [ 'a' ] => 111, [ 'a' ] => 222 }
+const set = new Set([
+    ['foo', 1],
+    ['bar', 2]
+])
+const m1 = new Map(set)
+// console.log(set);   //Set(2) { [ 'foo', 1 ], [ 'bar', 2 ] }
+console.log(m1);    //Map(2) { 'foo' => 1, 'bar' => 2 }
 
 
-// const m = new Map()
-// //0和-0 默认就是一个键
-// m.set(+0, 123)
-// m.set(-0, 123)
-// console.log(m); //Map(1) { 0 => 123 }
+const m = new Map()
+const k1 = ['a']
+const k2 = ['a']
+m.set(k1, 111).set(k2, 222)   //k1和k2值相同，在Map中也被认为是两个键
+console.log(m);     //Map(2) { [ 'a' ] => 111, [ 'a' ] => 222 }
 
 
-// const map = new Map()
-// map.set(true, 1)
-// map.set('true', 2)
-// map.set(undefined, 3)
-// map.set(null, 4)
-// map.set(NaN, 5)     //NaN默认就是一个键
-// map.set(NaN, 6)
-// console.log(map);   //Map(5) { true => 1, 'true' => 2, undefined => 3, null => 4, NaN => 6 }
+const m = new Map()
+//0和-0 默认就是一个键
+m.set(+0, 123)
+m.set(-0, 123)
+console.log(m); //Map(1) { 0 => 123 }
+
+
+const map = new Map()
+map.set(true, 1)
+map.set('true', 2)
+map.set(undefined, 3)
+map.set(null, 4)
+map.set(NaN, 5)     //NaN默认就是一个键
+map.set(NaN, 6)
+console.log(map);   //Map(5) { true => 1, 'true' => 2, undefined => 3, null => 4, NaN => 6 }
 
 
 const map = new Map([
@@ -88,8 +88,8 @@ map.forEach((e) => {
     // console.log(e);
 })
 
-let k = [...map.keys()]
-let val = [...map.values()]
+let k = [...map.keys()]     //keys() 返回键名的遍历器
+let val = [...map.values()] //values() 返回键值的遍历器
 let m2 = [...map]
 console.log(k);     //[ 'foo', 'bar' ]
 console.log(val);   //[ 'no', 'yes' ]
--- es6/set/index.js
+++ es6/set/index.js
@@ -1,90 +1,90 @@
 
-// const s = new Set()     //类数组
-// const arr = new Array()     ==const arr = []
-// console.log(s);     //Set(0) {}
-// let arr = [2, 3, 4, 5, 3, 4, 8]
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+const s = new Set()     //类数组
+const arr = new Array() == //const arr = []
+    console.log(s);     //Set(0) {}
+let arr = [2, 3, 4, 5, 3, 4, 8]
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
 
-// for (let item of arr) {
-//     console.log(item);
-// }
+for (let item of arr) {
+    console.log(item);
+}
 
-// arr.forEach((x) => {    //.forEach()遍历数组
-//     s.add(x)
-// })
-
-
-// const set = new Set([1, 2, 3, 4, 4])
-// console.log(set);   //Set(4) { 1, 2, 3, 4 }
-
-// const set = new Set([1, 2, 3, 4, 4, 5, 5])
-// console.log(set.size);   // 5
-
-// const set = new Set(document.querySelectorAll('div'))   //假如有两个div
-// console.log(set.size);      // 2
+arr.forEach((x) => {    //.forEach()遍历数组
+    s.add(x)
+})
 
 
-//数组去掉重复
-// let arr = [2, 3, 4, 5, 3, 4, 8]
-// console.log([...new Set(arr)]);     //[ 2, 3, 4, 5, 8 ]
+const set = new Set([1, 2, 3, 4, 4])
+console.log(set);   //Set(4) { 1, 2, 3, 4 }
+
+const set = new Set([1, 2, 3, 4, 4, 5, 5])
+console.log(set.size);   // 5
+
+const set = new Set(document.querySelectorAll('div'))   //假如有两个div
+console.log(set.size);      // 2
 
 
-// let arr = [2, 3, 4, 5, 3, 4, 8]//splice() 方法向/从数组中添加/删除项目，然后返回被删除的项目
-// console.log(arr.splice(3, 1));      //[5]
+// 数组去掉重复
+let arr = [2, 3, 4, 5, 3, 4, 8]
+console.log([...new Set(arr)]);     //[ 2, 3, 4, 5, 8 ]
 
 
-// let arr = [2, 3, 4, 5, 3, 4, 8]
-// console.log(arr.indexOf(5));    //下标为3
+let arr = [2, 3, 4, 5, 3, 4, 8]//splice() 方法向/从数组中添加/删除项目，然后返回被删除的项目
+console.log(arr.splice(3, 1));      //[5]
 
 
-//字符串去掉重复
-// console.log([...new Set('qweqweqwe')]);     //[ 'q', 'w', 'e' ]
+let arr = [2, 3, 4, 5, 3, 4, 8]
+console.log(arr.indexOf(5));    //返回3     //5的下标为3
 
 
-// const set = new Set()
-// set.add(5)
-// set.add('5')
-// console.log(set); Set(2) { 5, '5' }
-
-// const set = new Set()
-// let a = NaN
-// let b = NaN
-// set.add(a)
-// set.add(b)
-// console.log(set);   //Set(1) { NaN }
-
-// const set = new Set()
-// set.add({ name: 'hh' })
-// set.add({ name: 'hh' })
-// console.log(set);       // Set(2) { { name: 'hh' }, { name: 'hh' } }
+// 字符串去掉重复
+console.log([...new Set('qweqweqwe')]);     //[ 'q', 'w', 'e' ]
 
 
-// const set = new Set()
-// set.add(1).add(2).add(3)
-// set.delete(2)    删除元素为2
-// console.log(set);   //Set(2) { 1, 3 }
-// console.log(set.has(3));   //true    查看set里有没有元素3
-// set.clear()     //清空所有成员
-// console.log(set);   //Set(0) {}
+const set = new Set()
+set.add(5)
+set.add('5')
+console.log(set); //Set(2) { 5, '5' }
+
+const set = new Set()
+let a = NaN
+let b = NaN
+set.add(a)
+set.add(b)
+console.log(set);   //Set(1) { NaN }
+
+const set = new Set()
+set.add({ name: 'hh' })
+set.add({ name: 'hh' })
+console.log(set);       // Set(2) { { name: 'hh' }, { name: 'hh' } }
 
 
-// Array.from(['a', 'b', 'c']).forEach(e => {
-//     console.log(e);
-// })
+const set = new Set()
+set.add(1).add(2).add(3)
+set.delete(2)    //删除元素为2
+console.log(set);   //Set(2) { 1, 3 }
+console.log(set.has(3));   //true    查看set里有没有元素3
+set.clear()     //清空所有成员
+console.log(set);   //Set(0) {}
 
-// const set = new Set([1, 2, 3, 4])
-// const array = Array.from(set)
-// array.forEach(e => {
-//     console.log(e);
-// })
 
-// 数组去除重复
-// function de(array) {
-//     return Array.from(new Set(array))
-// }
-// console.log(de([1, 1, 2, 2, 3, 3]));
+Array.from(['a', 'b', 'c']).forEach(e => {
+    console.log(e);
+})
+
+const set = new Set([1, 2, 3, 4])
+const array = Array.from(set)
+array.forEach(e => {
+    console.log(e);
+})
+
+数组去除重复
+function de(array) {
+    return Array.from(new Set(array))
+}
+console.log(de([1, 1, 2, 2, 3, 3]));
 
 // set遍历
 let set = new Set(['qwe', 'rty', 'tyu'])
