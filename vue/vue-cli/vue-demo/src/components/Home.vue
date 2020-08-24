dabe78e0dd7e62a9d1026af69f5a749348972ea1
hcyn2000 <1520090739@qq.com>
2020-08-24 17:00:58 +0800

    vue

---
+++ vue/vue-cli/vue-demo/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <div class="name">哈哈哈</div>
+    <div class="age">18</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
