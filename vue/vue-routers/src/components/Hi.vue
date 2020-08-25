e805cb2c2446b2cd9aa46bd1166336b5b225731b
hcyn2000 <1520090739@qq.com>
2020-08-25 12:09:13 +0800

    vue

---
+++ vue/vue-routers/src/components/Hi.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+    <router-link :to="{name:'hi1',params:{name:'红尘'}}">hi 1</router-link>
+    <br />
+    <router-link to="/hi/hi2">hi 2</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hi",
+  data() {
+    return {
+      msg: "Hi, I am HHH",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue/vue-routers/src/components/Hi2.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="hi">我是hi 2页面</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
