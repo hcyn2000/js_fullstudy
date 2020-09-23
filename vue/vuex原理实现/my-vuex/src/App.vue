b567650d46ccc32e41884f04d82e64d48d41def5
hcyn2000 <1520090739@qq.com>
2020-09-23 21:37:15 +0800

    提交

---
+++ vue/vuex原理实现/my-vuex/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    {{ this.$store.state.count }}
+    <!-- 123 -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ vue/vuex原理实现/my-vuex/src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+// import Vuex from 'vuex'
+import Vuex from './myVuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    count: 1
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
