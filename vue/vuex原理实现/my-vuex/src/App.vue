0e3457e90eb05172084790882addbb9c3d99d5fc
hcyn2000 <1520090739@qq.com>
2020-09-24 16:54:26 +0800

    vuex原理

--- vue/vuex原理实现/my-vuex/src/App.vue
+++ vue/vuex原理实现/my-vuex/src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
     {{ this.$store.state.count }}
+    {{ this.$store.getters.getCount }}
+    <button @click="add">+1</button>
+    <button @click="reduce">+2</button>
     <!-- 123 -->
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    add() {
+      this.$store.commit("addCount", 1);
+    },
+    reduce() {
+      this.$store.dispatch("getReduceCount", 2);
+    },
+  },
+};
 </script>
 
 <style>
--- vue/vuex原理实现/my-vuex/src/store/index.js
+++ vue/vuex原理实现/my-vuex/src/store/index.js
@@ -8,9 +8,21 @@ export default new Vuex.Store({
   state: {
     count: 1
   },
+  getters: {
+    getCount: state => state.count * 100
+  },
   mutations: {
+    addCount(state, num) {
+      state.count += num
+    },
+    reduceCount(state, num) {
+      state.count -= num
+    }
   },
   actions: {
+    getReduceCount(context, num) {
+      context.commit("reduceCount", num)
+    }
   },
   modules: {
   }
