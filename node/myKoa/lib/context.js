cafcbbe588e94fd214d3a71943a54bfe8be1447e
hcyn2000 <1520090739@qq.com>
2020-10-21 14:27:32 +0800

    koa原理

--- node/myKoa/lib/context.js
+++ node/myKoa/lib/context.js
@@ -1,3 +1,21 @@
 let proto = {}
 
+function defineGetter(prop, name) {  // prop 代表对象    name 代表对象上的属性
+  proto.__defineGetter__(name, function () {  // 每个对象都有__defineGetter__方法，用它实现代理
+    return this[prop][name]   // url === 
+  })
+}
+
+function defineSetter(prop, name) {
+  proto.__defineSetter__(name, function (val) {  // 每个对象都有__defineGetter__方法，用它实现代理
+    this[prop][name] = val
+  })
+}
+
+defineGetter('request', 'url')
+defineGetter('request', 'path')
+defineGetter('response', 'body')
+
+defineSetter('response', 'body')
+
 module.exports = proto
