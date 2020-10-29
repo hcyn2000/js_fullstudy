ad11c2f6cc3d7582337c3e8231e731db43ea8459
hcyn2000 <1520090739@qq.com>
2020-10-29 14:47:27 +0800

    vue3.0

---
+++ vue/vue3.0/vue3-demo/readme.md
@@ -0,0 +1,12 @@
+# vue3.0 与vue2.0的差别
+
+1. template里面不再需要根节点，减少了没意义的dom
+2. 使用Composition API    (2.0使用的是options API)
+3. 完美支持treeshaking
+
+
+
+组件: 
+  fragment
+  teleport
+  suspense
---
+++ vue/vue3.0/vue3-demo/src/components/com-demo.vue
@@ -0,0 +1,18 @@
+<!-- fragment -->
+<template>
+  <ul>
+    <FragmentDemo :data="[1, 2, 3, 4, 5, 6, 7]" />
+  </ul>
+</template>
+
+<script>
+import FragmentDemo from "./fragment.vue";
+export default {
+  components: {
+    FragmentDemo,
+  },
+};
+</script>
+
+<style>
+</style>
