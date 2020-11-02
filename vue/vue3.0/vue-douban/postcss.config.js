74243d976b31eb25ae3c09361156c469f34dbf4b
hcyn2000 <1520090739@qq.com>
2020-11-02 12:18:30 +0800

    提交

---
+++ vue/vue3.0/vue-douban/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugin: {
+    'postcss-pxtorem': {
+      rootValue: 40.5,
+      propList: ['*']
+    }
+  }
+}
---
+++ vue/vue3.0/vue-douban/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import 'amfe-flexible/index.js'
+import './assets/styles/reset.css'
+
+createApp(App).use(store).use(router).mount('#app')
