7c4ce20979b299d71fa1f25624ba1b59bffaa06e
hcyn2000 <1520090739@qq.com>
2020-11-03 14:30:43 +0800

    vue3.0

--- vue/vue3.0/vue-douban/src/views/Home.vue
+++ vue/vue3.0/vue-douban/src/views/Home.vue
@@ -1,16 +1,48 @@
 <template>
   <div class="home">
-    <v-header></v-header>
+    <v-header title="豆瓣电影评分" :leftArrow="false"></v-header>
+    <MovieSetion :section="test"></MovieSetion>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
+import MovieSetion from "@/components/MovieSetion.vue";
+import { ref } from "vue";
 export default {
   name: "Home",
   components: {
     "v-header": Header,
+    MovieSetion,
+  },
+  setup() {
+    const test = ref({
+      movies: [
+        {
+          image:
+            "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2452075545.jpg",
+          title: "金刚川",
+          rating: { stars: [1, 1, 1, 0, 0], score: 6.5 },
+        },
+        {
+          image:
+            "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2452075545.jpg",
+          title: "金刚川",
+          rating: { stars: [1, 1, 1, 0, 0], score: 6.5 },
+        },
+        {
+          image:
+            "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2452075545.jpg",
+          title: "金刚川",
+          rating: { stars: [1, 1, 1, 0, 0], score: 6.5 },
+        },
+      ],
+    });
+
+    return {
+      test,
+    };
   },
 };
 </script>
