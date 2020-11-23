a054a8ae4a4ec8c9de596528475d74663a2ff244
hcyn2000 <1520090739@qq.com>
2020-11-23 21:46:16 +0800

    提交

---
+++ 面试题目/vue-mianshiti/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <keep-alive>
+      <HelloWorld @getData="getDataFromSon" msg="Welcome to Your Vue.js App" />
+    </keep-alive>
+
+    <input type="text" :value="value" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      value: "123",
+    };
+  },
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    getDataFromSon(data) {
+      console.log(data);
+    },
+  },
+  activated() {
+    // 命中缓存
+  },
+  deactivated() {},
+  beforeCreate() {
+    console.log("App beforeCreate");
+  },
+  created() {
+    console.log("App created");
+  },
+  beforeMount() {
+    console.log("App beforeMount");
+  },
+  mounted() {
+    console.log("App mounted");
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 面试题目/vue-mianshiti/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    Hello
+    <p>
+      {{ msg }}
+    </p>
+    <button @click="toParent">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  methods: {
+    toParent() {
+      // 子向父传值
+      this.$emit("getData", "来自子组件的值");
+    },
+  },
+  beforeCreate() {
+    console.log("Hello beforeCreate");
+  },
+  created() {
+    console.log("Hello created");
+  },
+  beforeMount() {
+    console.log("Hello beforeMount");
+  },
+  mounted() {
+    console.log("Hello mounted");
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
