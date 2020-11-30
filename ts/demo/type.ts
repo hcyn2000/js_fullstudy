0823aa2341456fd431ea7fe7785c935da1eb1c14
hcyn2000 <1520090739@qq.com>
2020-11-30 14:31:45 +0800

    TS

--- ts/demo/type.ts
+++ ts/demo/type.ts
@@ -1,4 +1,3 @@
-import { dir } from "console"
 
 let isDone: boolean = false // Boolean类型
 // isDone = 123 // 报错
@@ -22,21 +21,70 @@ let list: Array<number> = [1, 2, 3]   // 泛型定义(写法二)
 
 // Enum
 // 数字枚举
-// enum Direction {
-//   EAST = 3,
-//   SOUTH,
-//   WEST,
-//   NORTH
-// }
+enum Direction {
+  EAST,
+  SOUTH,
+  WEST,
+  NORTH
+}
 
-// let dir: Direction = Direction.NORTH  // 6
-console.log(dir);
+let dir: Direction = Direction.NORTH  // 3
+let dirVal = Direction[0] // EAST
+console.log(dir, dirVal);
 
 
 // 字符串枚举
-enum Direction {
-  EAST = 'EAST',
-  SOUTH = 'SOUTH',
-  WEST = 'WEST',
-  NORTH = 'NORTH'
+// enum Direction {
+//   EAST = 'EAST',
+//   SOUTH = 'SOUTH',
+//   WEST = 'WEST',
+//   NORTH = 'NORTH'
+// }
+
+
+// 异构枚举
+enum Enum {
+  A,
+  B,
+  C = 'c',
+  D = 'd',
+  E = 8,
+  F
 }
+
+
+// any  可以被任何类型赋值，可以被赋值成任何类型  不确定是什么类型的时候可以用any 尽量不要用
+let notSuer: any = 666
+notSuer = 'hello'
+
+let test: number = notSuer
+
+
+// unKnown  可以被任何类型赋值，不可以被赋值成任何类型，
+let value: unknown
+value = true
+value = 123
+value = 'hello'
+
+let value1: unknown = value
+let value2: any = value
+// let value3:number = value // 报错
+// let value4:string = value // 报错
+
+
+// tuple类型  元组
+let tupleType: [string, boolean]
+tupleType = ['yn', true]
+
+
+// void类型
+function User(): void {
+  console.log('hello');
+}
+
+
+// null undefined
+let u: undefined = undefined
+let n: null = null
+
+let num: number = u
